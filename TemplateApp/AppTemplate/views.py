9b33f97f6391e61413c36394ab476925febb4cbf
javier <javierbravoconde@gmail.com>
2014-04-05 19:38:42 +0200

    first version working

--- TemplateApp/AppTemplate/views.py
+++ TemplateApp/AppTemplate/views.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response,get_object_or_404, render
 from django.template import RequestContext
 from forms import ContactForm
+import json
 
 
 def viewtemplate(request):
@@ -15,10 +16,14 @@ def viewtemplate(request):
             
             
     else:
-        inputvalue = request.GET.get('inputvalue')
-                
+        inputvalue = request.GET.get('inputvalue')                
         form = ContactForm({'inputvalue':inputvalue, 'outputvalue':inputvalue })
+        
+        var_array_tuple = [('a',1), ('b',2)]
+        string_json = json.dumps(var_array_tuple)
+        
 
     return render(request, 'template.html', {
+        'string_json':string_json,
         'form': form,
     })    
