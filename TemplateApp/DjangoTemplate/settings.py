dcd78bf299aacc1ea07979c17fd6378eed5c356d
javier <javierbravoconde@gmail.com>
2014-04-05 13:11:08 +0200

    settings fixed

--- TemplateApp/DjangoTemplate/settings.py
+++ TemplateApp/DjangoTemplate/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/javier/Documents/Aptana Studio 3 Workspace/DjangoTemplate/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/javier/django-highcharts/TemplateApp/sqlite.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -107,7 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/javier/Documents/Aptana Studio 3 Workspace/DjangoTemplate/AppTemplate/templates',
+    '/home/javier/django-highcharts/TemplateApp/AppTemplate/templates',
 )
 
 INSTALLED_APPS = (
